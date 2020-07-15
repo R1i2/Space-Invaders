c79453b307bccdeee9f23572a3f308b75045400f
Rishav <rishavraj8967@gmail.com>
2020-07-15 09:47:39 +0530

    Initialize pygame and game loop

---
+++ file1.py
@@ -0,0 +1,14 @@
+import pygame
+
+#Initialize the pygame
+pygame.init()
+
+#crate the screen
+screening=pygame.display.set_mode((800,600))
+
+#Game loop
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
